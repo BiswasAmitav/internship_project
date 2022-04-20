a40488d5129a03f80f02b4a94e38f3da3c3b528b
BiswasAmitav <biswasamit2648@gmail.com>
2022-04-20 11:13:30 +0530

    jobs portal page has benn created

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php include 'header.php' ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,7 +13,78 @@
     <!-- EXTERNAL CSS LINK -->
     <link rel="stylesheet" href="style\style.css">
     <style>
-        
+        /* The side navigation menu */
+        .sidebar {
+            margin: 0;
+            margin-top: 8px;
+            padding: 0;
+            width: 200px;
+            background-color: #f1f1f1;
+            position: fixed;
+            height: 100%;
+            overflow: auto;
+        }
+
+        /* Sidebar links */
+        .sidebar a {
+            display: block;
+            color: black;
+            padding: 16px;
+            text-decoration: none;
+        }
+
+        /* Active/current link */
+        .sidebar a.active {
+            background-color: #04AA6D;
+            color: white;
+        }
+
+        /* Links on mouse-over */
+        .sidebar a:hover:not(.active) {
+            background-color: #555;
+            color: white;
+        }
+
+        /* Page content. The value of the margin-left property should match the value of the sidebar's width property */
+        div.content {
+            margin-top: 60px;
+            margin-left: 200px;
+            padding: 1px 16px;
+            height: 1000px;
+        }
+
+        /* On screens that are less than 700px wide, make the sidebar into a topbar */
+        @media screen and (max-width: 700px) {
+            .sidebar {
+                width: 100%;
+                height: auto;
+                position: relative;
+            }
+
+            .sidebar a {
+                float: left;
+            }
+
+            div.content {
+                margin-left: 0;
+            }
+        }
+
+        /* On screens that are less than 400px, display the bar vertically, instead of horizontally */
+        @media screen and (max-width: 400px) {
+            .sidebar a {
+                text-align: center;
+                float: none;
+            }
+        }
+
+        .navbar {
+            overflow: hidden;
+            background-color: #333;
+            position: fixed;
+            top: 0;
+            width: 100%;
+        }
     </style>
     <title>Dashboard</title>
 </head>
@@ -20,12 +93,12 @@
     <div>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="#">Admin Dashboard</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
+                    <!-- <ul class="navbar-nav">
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
@@ -38,13 +111,13 @@
                         <li class="nav-item">
                             <a class="nav-link disabled">Disabled</a>
                         </li>
-                    </ul>
+                    </ul> -->
                 </div>
             </div>
         </nav>
         <div class="sidebar">
-            <a class="active" href="#home">Home</a>
-            <a href="#news">News</a>
+            <a class="active" href="#home">Jobs</a>
+            <a href="#jobs.php">Candidates Applied</a>
             <a href="#contact">Contact</a>
             <a href="#about">About</a>
         </div>
@@ -62,7 +135,7 @@
                     Link with href
                 </a> -->
                 <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                Post Job
+                    Post Job
                 </button>
             </p>
             <div class="collapse" id="collapseExample">
@@ -94,36 +167,36 @@
                 </div>
             </div>
             <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Company Name</th>
-      <th scope="col">Position</th>
-      <th scope="col">CTC</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>TCS</td>
-      <td>Software Developer</td>
-      <td>8 LPA</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Google</td>
-      <td>Digital Marketing</td>
-      <td>12 LPA</td>
-    </tr>
-    <!-- <tr>
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Company Name</th>
+                        <th scope="col">Position</th>
+                        <th scope="col">CTC</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>TCS</td>
+                        <td>Software Developer</td>
+                        <td>8 LPA</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Google</td>
+                        <td>Digital Marketing</td>
+                        <td>12 LPA</td>
+                    </tr>
+                    <!-- <tr>
       <th scope="row">3</th>
       <td colspan="2">Larry the Bird</td>
       <td>@twitter</td>
     </tr> -->
-  </tbody>
-</table>
+                </tbody>
+            </table>
         </div>
-        
+
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
---
+++ jobs.php
@@ -0,0 +1,33 @@
+<?php include 'header.php' ?>
+
+<div class="content">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Candidate Name</th>
+                <th scope="col">Position</th>
+                <th scope="col">Resume</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Ram</td>
+                <td>Php Developer</td>
+                <td ><a href=""><i class='bx bxs-download'></i></a></td>
+            </tr>
+            <!-- <tr>
+                <th scope="row">2</th>
+                <td>Google</td>
+                <td>Digital Marketing</td>
+                <td>12 LPA</td>
+            </tr> -->
+            <!-- <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr> -->
+        </tbody>
+    </table>
+</div>
