af64934be1a93b0bd74c06901df0525f1487947b
BiswasAmitav <biswasamit2648@gmail.com>
2022-04-22 13:05:25 +0530

    database connector i creted by this config page & related work has done

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$server='localhost';
+$username='root';
+$password='';
+$database='jobs';
+
+
+$conn=mysqli_connect($server,$username,$password,$database);
+
+
+?>
--- register.php
+++ register.php
@@ -27,19 +27,19 @@
 
 <body>
     <div class="container">
-        <form>
+        <form action="">
             <div class="mb-3">
                 <label for="exampleInputName" class="form-label">Full Name</label>
                 <input type="text" class="form-control" id="exampleInputName">
             </div>
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
                 <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div class="mb-3">
                 <label for="exampleInputNumber" class="form-label">Phone Number</label>
-                <input type="Number" class="form-control" id="exampleInputNumber">
+                <input type="Number" class="form-control" id="exampleInputNumber" name="phone_no">
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
@@ -47,13 +47,14 @@
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword2" class="form-label">Confirm Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword2">
+                <input type="password" class="form-control" id="exampleInputPassword2" name="password">
             </div>
             <!-- <div class="mb-3 form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
                 <label class="form-check-label" for="exampleCheck1">Check me out</label>
             </div> -->
             <button type="submit" class="btn btn-primary">Submit</button>
+            <!-- <input type="submit" class="btn btn-primary" value="Submit"> -->
             <br>
             Already Registered? <a style="text-decoration: none;" href="Login.php">Login</a>
         </form>
